00bbbb5c59c828bcc92f13830fa2b627103f51c2
Scepti <40700483+Scepti@users.noreply.github.com>
2018-07-06 15:24:58 -0400

    Update time.py

--- commands/time.py
+++ commands/time.py
@@ -223,7 +223,9 @@ async def time(message, args):
       await client.send_message(message.channel, str(Hour) + str(":") + str(Min))
     if Min < 10:
       await client.send_message(message.channel, str(Hour) + str(":") + str("0") + str(Min))
-  elif len(args) == 0
+  elif args == 'why':
+    await client.send_message(message.channel, 'https://youtu.be/-5wpm-gesOY')
+  elif len(args) == 0:
     await client.send_message(message.channel, "Valid timezones include: gmt, cat, agt, bet, est, iet, prt, cst, mst, pst, pnt, ast, hst, mit, nst, sst, aet, jst, kst, ctt, vst, plt, net, eat, art, cet, eet, bst, utc. Please contact Scepti if you would like a timezone added. ^_^")
   else:
     await client.send_message(message.channel, str(args) + "is not a valid timezone, please use `!time` for a list of included timezones.")
