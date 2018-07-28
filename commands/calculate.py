ba65c4feca9e205ee323c412b6461ecee6ded8ae
Hyla Aster <sethvale@mail.com>
2018-07-28 18:58:04 -0500

    Allows parenthesis and spaces in calculate command.

--- commands/calculate.py
+++ commands/calculate.py
@@ -1,7 +1,8 @@
 from header import *
 async def calculate(message, args):
-  allowed_chars = ['+','-','*','/','^','0','1','2','3','4','5','6','7','8','9','.']
+  allowed_chars = ['+','-','*','/','^','0','1','2','3','4','5','6','7','8','9','.','(',')']
   if all([c in allowed_chars for c in args]):
+	args=string.replace(" ","")
     try:
       args = eval(args)
     except:
