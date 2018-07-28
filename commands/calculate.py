b6e66fb566360333d4a9f65df3f2180633c88455
Scepti <40700483+Scepti@users.noreply.github.com>
2018-07-28 18:38:48 -0500

    Update calculate.py

--- commands/calculate.py
+++ commands/calculate.py
@@ -1,4 +1,13 @@
 from header import *
 async def calculate(message, args):
-  args = eval(args)
-  await client.send_message(message.channel, args)
+  allowed_chars = ['+','-','*','/','^','0','1','2','3','4','5','6','7','8','9','.']
+  if all([c in allowed_chars for c in args]):
+    try:
+      args = eval(args)
+      break
+    except:
+      await client.send_message(message.channel, "Please check your syntax and try again")
+      return
+    await client.send_message(message.channel, args)
+  else:
+    await client.send_message(message.channel, "Please only include numbers and mathematical operations")
