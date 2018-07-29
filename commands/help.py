85c21462373aa6b965a15542332a776881617817
Hyla Aster <sethvale@mail.com>
2018-07-28 21:22:19 -0500

    Changes the format of the help command to use a Discord embed

--- commands/help.py
+++ commands/help.py
@@ -4,7 +4,7 @@ async def help(message, args):
   if user == None:
     user = message.author
   if haspermission(user):
-    await client.send_message(user, help_message)
+    await client.send_message(user, embed=help_message)
     await client.send_message(user, help_message_mod)
   else:
-    await client.send_message(user, help_message)
+    await client.send_message(user, embed=help_message)
