de453d72174580451215c9f348b117642da52bef
Rishabh2 <rishabh.bhasin.33@gmail.com>
2018-08-01 13:42:38 -0700

    Delete custom.py

--- commands/custom.py
+++
@@ -1,7 +0,0 @@
-from header import *
-async def custom(message, args):
-  if haspermission(message.author):
-    user = getmention(message)
-    if user == None:
-      user = message.author
-    await client.send_message(user, embed=singles_embed)
