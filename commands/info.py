671e1499b5f2c6ea8a3491b8116a4f639fb62eed
Scepti <40700483+Scepti@users.noreply.github.com>
2018-08-14 23:53:06 -0500

    Update info.py

--- commands/info.py
+++ commands/info.py
@@ -2,14 +2,26 @@ from header import *
 async def info(message, args):
   user = getmention(message)
   if user == None:
-    user = message.author
-  userid = discorduser_to_id(user)
+    if len(args) == 0:
+      userid = discorduser_to_id(message.author)
+    else:
+      cursor.execute(reddit_select_name_str, (args.lower(),))
+      result = cursor.fetchone()
+      if result == None or result[0] == None:
+        await client.send_message(message.channel, 'There is no one on this server with the reddit username ' + args)
+        return
+      else:
+        userid = result[0]
+  else:
+    userid = discorduser_to_id(user)
+    
   cursor.execute(info_dump_str, (userid,))
   result = cursor.fetchone()
-  if result == None:
-    await client.send_message(message.channel, 'User has no info')
-  else:
-    embed = discord.Embed(title="User Info", color=discord.Color(0x85bff8))
+  embed = discord.Embed(title="User Info", color=discord.Color(0x85bff8))
+  user = id_to_discorduser(userid) #I think this part might work but its inefficent I think, then again I didn't actually read any of your custom functions so I don't know...
+  name = discorduser_to_discordname(user)
+  embed.add_field(name='Discord Name', value=name)  
+  if result != None:
     if result[1] != None:
       embed.add_field(name='Friend Code', value=result[1])
     if result[2] != None:
@@ -29,4 +41,4 @@ async def info(message, args):
     if result != None and len(result) > 0:
       for pair in result:
         embed.add_field(name='TSV: '+pair[0], value=pair[1])
-    await client.send_message(message.channel, embed=embed)
+  await client.send_message(message.channel, embed=embed)
