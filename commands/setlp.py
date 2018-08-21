802c72da3f1fcbe4d79dbc4761daa14c2cff64bb
Rishabh <risabh.bhasin.33@gmail.com>
2018-08-21 02:22:08 +0000

    Added files that were not added previously

---
+++ commands/setlp.py
@@ -0,0 +1,30 @@
+from header import *
+async def setlp(message, args):
+  c = message.channel
+  await client.send_message(c,'Time to set up your league pass!\nYou start with 6 main pokemon, and will unlock 4 sideboard slots as you continue.\nAs a reminder, legendary pokemon are not allowed in our format. The full details can be found on the subreddit wiki.')
+  retry = False
+  while not retry:
+    await client.send_message(c, 'Now, enter the names of your first 6 pokemon:')
+    await client.send_message(c, 'Type "cancel" to cancel')
+    mons = []
+    for _ in range(6):
+      resp = await client.wait_for_message(timeout=60, author=message.author, channel=c)
+      if resp == None or resp.content.lower() == 'cancel':
+        await client.send_message(c, 'Bye')
+        return
+      mons.append(resp.content)
+    await client.send_message(c, 'Alright, your pokemon are: ' + ', '.join(mons))
+    await client.send_message(c, 'Is this correct? (y/n)')
+    resp = await client.wait_for_message(timeout=60, author=message.author, channel=c)
+    if resp == None:
+      await client.send_message(c, 'Bye')
+      return
+    retry = resp.content.lower()[0] == 'y'
+  cursor.execute('SELECT * FROM betalp WHERE id=?', (message.author.id,))
+  result = cursor.fetchone()
+  if result == None:
+    cursor.execute('INSERT INTO betalp (id, mon1, mon2, mon3, mon4, mon5, mon6) VALUES (?,?,?,?,?,?,?)', (message.author.id, *mons))
+  else:
+    cursor.execute('UPDATE betalp SET mon1=?, mon2=?, mon3=?, mon4=?, mon5=?, mon6=?) WHERE id=?', (*mons, message.author.id))
+  connection.commit()
+  await client.send_message(c, 'All Done!')
