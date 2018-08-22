1e845c160e873c1487335ffc38fef298e0bd341c
Rishabh <risabh.bhasin.33@gmail.com>
2018-08-22 07:21:44 +0000

    Friendlier messages in setlp

--- commands/setlp.py
+++ commands/setlp.py
@@ -1,6 +1,9 @@
 from header import *
 async def setlp(message, args):
   c = message.channel
+  if c.id != '481721487569453076':
+    await client.send_message(c, 'Please only use !setlp in the <#481721487569453076> channel')
+    return
   await client.send_message(c,'Time to set up your league pass!\nYou start with 6 main pokemon, and will unlock 4 sideboard slots as you continue.\nAs a reminder, legendary pokemon are not allowed in our format. The full details can be found on the subreddit wiki.')
   retry = False
   while not retry:
@@ -14,8 +17,9 @@ async def setlp(message, args):
         return
       if resp.content in pokemon_list:
         mons.append(resp.content)
+        await client.send_message(c, 'Added ' + resp.content)
       else:
-        await client.send_message(c, 'That is not a Pokemon I know. Double check spelling and capitalization, and you may need to specfiy form')
+        await client.send_message(c, resp.content + ' is not a Pokemon I know. Double check spelling and capitalization, and you may need to specfiy form')
     await client.send_message(c, 'Alright, your pokemon are: ' + ', '.join(mons))
     await client.send_message(c, 'Is this correct? (y/n)')
     resp = await client.wait_for_message(timeout=60, author=message.author, channel=c)
