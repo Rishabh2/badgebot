8dcad543a0ef43292a90e10b32b49f988921d9b7
Rishabh <risabh.bhasin.33@gmail.com>
2018-08-25 00:59:46 +0000

    Added Roster

--- commands/help.py
+++
@@ -1,8 +0,0 @@
-from header import *
-async def help(message, args):
-  user = getmention(message)
-  if user == None:
-    user = message.author
-  await client.send_message(user, embed=help_message)
-  if haspermission(user):
-    await client.send_message(user, embed=help_message_mod)
