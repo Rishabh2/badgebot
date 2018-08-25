46f48debc48b254fa0f881b1c54ee1740dac85e5
Rishabh <risabh.bhasin.33@gmail.com>
2018-08-25 01:09:28 +0000

    Added Roster

--- commands/est.py
+++
@@ -1,3 +0,0 @@
-from header import *
-async def est(message, args):
-  await client.send_message(message.channel, datetime.datetime.now(tz=datetime.timezone(datetime.timedelta(hours=-  4))).strftime('%I:%M%p'))
