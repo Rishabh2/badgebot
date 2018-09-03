f3dd94bde787eaf229c9b7cff5653b011b693538
Rishabh <risabh.bhasin.33@gmail.com>
2018-09-03 22:39:45 +0000

    Cleaner draft list

--- commands/draft.py
+++ commands/draft.py
@@ -14,10 +14,15 @@ async def draft(message, args):
   mon_col = TEAM_COLS[result[1]]
   point_col = POINT_COLS[result[1]]
 
-  roster = service.spreadsheets().values().get(spreadsheetId=sheet, range='Rosters', majorDimension='COLUMNS').execute()
-  roster = roster.get('values', [])[result[1]*3+2]
-  msg = '\n'.join([r for r in roster if r != None])
-  await client.send_message(message.channel, msg)
+  rosterpage = service.spreadsheets().values().get(spreadsheetId=sheet, range='Rosters', majorDimension='COLUMNS').execute()
+  values = rosterpage.get('values', [])
+  roster = values[result[1]*3+2][:-1]
+  labels = values[1][:-1]
+  embed = discord.Embed(title="Draft Info", color=discord.Color(0x85bff8))
+  for s in zip(labels, roster):
+    if s[0] != '' and s[1] != '':
+      embed.add_field(name=s[0], value=s[1])
+  await client.send_message(message.channel, embed=embed)
 
 #   # Get all the cells of pokemon tiers
 #   tiers = service.spreadsheets().values().get(spreadsheetId=sheet, range='Tiers', majorDimension='COLUMNS').execute()
