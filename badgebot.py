66ae9e1ca77cd017dd9d83b8a15fafcb791e31b2
Rishabh <risabh.bhasin.33@gmail.com>
2018-10-29 19:12:02 +0000

    Fixed small typo

--- badgebot.py
+++ badgebot.py
@@ -236,7 +236,7 @@ async def on_message(message):
     await client.send_message(message.channel, embed=discord.Embed(color=discord.Color(0xbc614e)).set_image(url=sprite_url.format('farfetchd')))
   if any([m.id=='213008672610189312' for m in message.mentions]):
     await client.send_message(message.channel, 'Ponged!')
-  if any([m.id=='260515473999921162' for m in message.mentions]):
+  if any([m.id=='282638912227115008' for m in message.mentions]):
     await client.send_message(message.channel, '<:BigFella:458459366782271488>')
 
   if len(text) > 0 and text[0] == '!':
