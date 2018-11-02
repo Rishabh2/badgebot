b3b2bb772851a3b649d106232ac2c61a73de6e1d
Rishabh <risabh.bhasin.33@gmail.com>
2018-11-02 17:30:05 +0000

    Better challenge time output format

--- commands/challengetime.py
+++ commands/challengetime.py
@@ -11,8 +11,10 @@ async def challengetime(message, args):
     recent_time = recent_time[0] if recent_time != None else 0
     current_time = int(message.timestamp.timestamp())
     time_dif = current_time - recent_time
-    if time_dif > challenge_time_limit:
-      msg = 'You have ' + str(time_dif) + ' seconds remaining'
+    time_remaining = challenge_time_limit - time_dif
+    hours = time_remaining / (60 * 60)
+    if time_remaining > 0:
+      msg = 'You have {:01.1f} hours remaining'.format(hours)
     else:
       msg = 'You are ready to submit a challenge'
   await client.send_message(message.channel, msg)
