478bad5d7ef7266b2cd53fa0cdac13e10ec84d74
Rishabh <risabh.bhasin.33@gmail.com>
2018-11-14 18:44:38 +0000

    Messages for miso

--- commands/message.py
+++ commands/message.py
@@ -1,6 +1,6 @@
 from header import *
 async def message(message, args):
-  if message.author.id == '242558859300831232':
+  if message.author.id == '242558859300831232' or message.author.id == '159118090519445514':
     (userid, messagetext) = args.split(maxsplit=1)
     target = client.get_channel(userid)
     msg = (target, messagetext)
